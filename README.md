040c1f837fa49c9a40a30a02732bc4932f61f8e4
tinaserra <vserra@student.42.fr>
2020-10-06 13:25:36 +0200

    Learn Asm Bitches

--- README.md
+++ README.md
@@ -1,9 +1,62 @@
 # libASM
+
 Writing a lib in x86 assembly (intel flavour)
 
 ## INTRODUCTION 🤓
 
 Sujet -> [EN](https://github.com/tinaserra/Cub3D/blob/master/links/cub3d_en.pdf) | [FR](https://github.com/tinaserra/Cub3D/blob/master/links/cube3d_fr.pdf)
 
+### L'assembleur, c'est quoi?
+
+C'est un ensemble de langages de programmation qui sont spécifiques à l'architechture de notre processeur. On peut dire qu'il y a autant d'assembleur que d'architechtures.
+
+```
+Expmples : mips - armv7 - x86 - x86-64 ou x64
+```
+
+### Pourquoi apprendre l'assembleur en 2021?
+
+Pour mieux connaitre son ordinateur et son foctionnement. Également pour reverse des programmes.
+
+### Syntaxes
+
+On a deux syntaxes en asm :
+
+* At&t : instruction $0x1, %eax (source, destination)
+* Intel : instruction eax, 0x1  (destination, source)
+
+## STRUCTURE 🦄
+
+### Segements
+
+Les **segements** sont des sortes de boites dans les quelles on va mettre des **valeurs** ou des **instructions**. Le programme nasm ira chercher dans ces segments les valers dont il connait la nature.
+
+```.bss```  = variables non initialisées : ```int a = 0;```</br>
+```.data``` = variables initialisées : ```int a = 1;```</br>
+```.text``` = code executable : ```printf("%d", a);```
+
+⚠️ Segments ≠ Sections ⚠️
+
+Petit paradoxe :</br>
+
+Pour déclarer un segment on utilise le mot ```section```
+
+```s
+section .bss
+    a resb 256
+
+section .data
+    hello db `hello world\n`
+    hello_len equ $-hello 
+
+section .text
+    global _start ; l'EP ici
+```
+
+### Registres
+
+### Instructions
+
 ## USEFUL LINKS 🤙🏼
+
 * [Raycasting tutorial video](https://courses.pikuma.com/courses/take/raycasting/lessons/7485598-introduction-and-learning-outcomes)
---
+++ learn_asm/main.asm
@@ -0,0 +1,73 @@
+; [Assembleur] : 
+;    mips - armv7 - x86 - x86-64 ou x64
+
+; At&t : instruction $0x1, %eax
+; Intel : instruction eax, 0x1
+
+;   [Segment]
+;       .bss = variables non initialisées : int a; == int a = 0, char buf[256];
+;       .data = variables initialisées int a = 1;
+;       .text = code executable : printf("%d", a);
+;
+;   [Registres]
+;       ax -> Eax (Extended ax) -> rax
+;       bx -> Ebx -> rbx
+;       cx -> Ecx -> rcx
+;       dx -> Edx -> rdx
+;       si -> Esi -> rsi # src
+;       di -> Edi -> rsi
+;       sp -> Esp -> rsp
+;       bp -> Ebp -> rbp
+;       ip -> Eip -> rip
+;
+;   [Instructions]
+;       mov : met une valeur dans un registre.
+;       db  : déclare un byte (octet).
+;       lea : met l'addresse d'une variable dans la destination. 
+
+
+BITS 64
+
+section .bss
+    a resb 256
+
+section .data
+    hello db `hello world\n`
+    hello_len equ $-hello 
+
+section .text
+    global _start ; l'EP ici
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hello
+    mov rdx, hello_len
+    syscall
+    mov rax, 123
+    mov [a], rax
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+
+;   -----------------------------------------###################################################------------------------------------------
+
+;   # II - Arguments utilisateurs et manipulation de la stack
+;       Equivalent de argc et argv
+
+; [Registres]
+;       rsp -> pointe vers la derniere valeur empilée sur la stack (addr basses)
+;       rbp -> pointe vers la base de la stack (addr hautes), sert de référence pour les fonctions
+;       rip -> pointe vers la prochaine instruction
+;       r8 -> Registres généraux
+;       r9 -> Registres généraux
+;       r10 -> Registres généraux
+;       r11 -> Registres généraux
+;       r12 -> Registres généraux
+;       r13 -> Registres généraux
+;       r14 -> Registres généraux     
+;       r15 -> Registres généraux
+
+;   -----------------------------------------###################################################------------------------------------------
